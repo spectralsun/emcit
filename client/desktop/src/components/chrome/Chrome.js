125631c4127bd64f21dcaaa37c2bc94d71216111
spectralsun <garrett@spectralsun.com>
2017-04-09 02:47:14 -0700

    ui improvements

--- client/desktop/src/components/chrome/Chrome.js
+++ client/desktop/src/components/chrome/Chrome.js
@@ -3,6 +3,7 @@ import AppBar from 'react-toolbox/lib/app_bar';
 import Navigation from 'react-toolbox/lib/navigation';
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
+import cx from 'classnames'
 
 import classes from './Chrome.css'
 
@@ -10,26 +11,37 @@ class Chrome extends React.Component {
 
     render() {
         const { user } = this.props;
+        const { pathname } = this.props.routing.locationBeforeTransitions;
+        const isAdmin = user && user.role === 'admin';
+        const isAnalyst = user && user.role === 'analyst';
         return (
             <div>
-                <AppBar title="Emcit Desktop" fixed>
+                <AppBar title="Emerald Citizen" fixed>
                     <Navigation type="horizontal" className={classes.centerNav}>
-                        {user &&
-                            <div data-react-toolbox="link">
+                        {isAnalyst &&
+                            <div className={cx({[classes.activeNav]: pathname === '/reports/table'})} data-react-toolbox="link">
                                 <Link to="/reports/table">Table</Link>
                             </div>
                         }
-                        {user &&
-                            <div data-react-toolbox="link">
+                        {isAnalyst &&
+                            <div className={cx({[classes.activeNav]: pathname === '/reports/map'})} data-react-toolbox="link">
                                 <Link to="/reports/map">Map</Link>
+                            </div>
+                        }
+                        {isAdmin &&
+                            <div className={cx({[classes.activeNav]: pathname === '/users/new'})}  data-react-toolbox="link">
+                                <Link to="/users/new">New User</Link>
+                            </div>
+                        }
+                        {isAdmin &&
+                            <div className={cx({[classes.activeNav]: pathname === '/users'})}  data-react-toolbox="link">
+                                <Link to="/users">Users</Link>
                             </div>
                         }
                     </Navigation>
                     <Navigation type="horizontal">
-                        {user && <div data-react-toolbox="link">{user.name}</div>}
-                        {user && <div data-react-toolbox="link"><Link to="/users/new">New User</Link></div>}
-                        {user && <div data-react-toolbox="link"><Link to="/users">Users</Link></div>}
-                        {user && <div data-react-toolbox="link"><a href="/logout">Logout</a></div>}
+                        {user && <div className={classes.userInfo} data-react-toolbox="link" className={classes.userInfo}>{user.name}</div>}
+                        {user && <div className={classes.logoutButton} data-react-toolbox="link"><a href="/logout">Logout</a></div>}
                     </Navigation>
                 </AppBar>
                 <div className={classes.views}>
@@ -40,6 +52,6 @@ class Chrome extends React.Component {
     }
 }
 
-const mapStateToProps = ({ account: { user }}) => ({ user });
+const mapStateToProps = ({ account: { user }, routing }) => ({ user, routing });
 
 export default connect(mapStateToProps, {})(Chrome);
