13b811ae0fac460ee94a44deb5a8c34097ca7f76
slively <scott.lively@gmail.com>
2017-05-21 12:51:07 -0700

    add lots more vehicle make & models

--- client/common/components/form/VehicleForm.js
+++ client/common/components/form/VehicleForm.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Button from 'react-toolbox/lib/button';
 import Dropdown from 'react-toolbox/lib/dropdown';
 import Input from 'react-toolbox/lib/input';
 
-import FormErrors from './FormErrors';
 import Form from './Form';
 import {
     MAKE_OPTIONS, MODEL_OPTIONS, ALL_MODEL_OPTIONS, COLOR_OPTIONS
@@ -12,7 +10,7 @@ import {
 export default class VehicleForm extends React.Component {
     setMake = make => {
         const { model } = this.props.vehicle;
-        if (model && !MODEL_OPTIONS[make].some(m => m.value === model)) {
+        if (model && !MODEL_OPTIONS[make].some(m => m === model)) {
             this.props.onChange('model', null);
         }
         this.props.onChange('make', make);
@@ -20,7 +18,7 @@ export default class VehicleForm extends React.Component {
 
     setModel = model => {
         if (!this.props.vehicle.make) {
-            this.props.onChange('make', ALL_MODEL_OPTIONS.find(m => m.value === model).make);
+            this.props.onChange('make', ALL_MODEL_OPTIONS.find(m => m === model).make);
         }
         this.props.onChange('model', model);
     }
--- client/common/consts/vehicleOptions.js
+++ client/common/consts/vehicleOptions.js
@@ -1,30 +1,439 @@
-export const MAKE_OPTIONS = [
-    { value: 'ford', label: 'Ford' },
-    { value: 'honda', label: 'Honda' },
-    { value: 'subaru', label: 'Subaru' }
-]
-
-export const MODEL_OPTIONS = {
-    ford: [
-        { value: 'f150', label: 'F150' },
-        { value: 'pinto', label: 'Pinto' }
-    ],
-    honda: [
-        { value: 'accord', label: 'Accord' },
-        { value: 'civic', label: 'Civic' }
-    ],
-    subaru: [
-        { value: 'forester', label: 'Forester'},
-        { value: 'outback', label: 'Outback'}
-    ]
-}
-
-export const ALL_MODEL_OPTIONS = Object.keys(MODEL_OPTIONS).reduce((arr, make) =>
-    arr.concat(MODEL_OPTIONS[make].map(model => Object.assign({}, model, { make })))
-, [])
-
 export const COLOR_OPTIONS = [
     { value: 'blue', label: 'Blue' },
     { value: 'red', label: 'Red' },
     { value: 'yellow', label: 'Yellow'}
 ]
+
+export const MODEL_OPTIONS = {
+    "Chrysler": [
+        "300",
+        "pt cruiser",
+        "crossfire",
+        "voyager",
+        "town and country",
+        "300C",
+        "sebring",
+        "LeBaron",
+        "pacifica",
+        "cirrus",
+        "concorde",
+        "180",
+        "200"
+    ],
+    "Dodge": [
+        "avenger",
+        "caliber",
+        "challenger",
+        "charger",
+        "colt",
+        "dart",
+        "Intrepid",
+        "magnum",
+        "neon",
+        "shadow",
+        "stealth",
+        "stratus",
+        "spirit",
+        "stratus",
+        "viper"
+    ],
+    "Jeep": [
+        "cherokee",
+        "grand cherokee",
+        "compass",
+        "wrangler",
+        "patiort",
+        "liberty",
+        "commander",
+        "wagoneer",
+        "commander"
+    ],
+    "Ford": [
+        "fiesta",
+        "focus",
+        "fusion",
+        "taurus",
+        "fiesta",
+        "mustang",
+        "b-max",
+        "c-max",
+        "s-max",
+        "galaxy",
+        "ecosport",
+        "flex",
+        "edge",
+        "explorer",
+        "expedition",
+        "ranger",
+        "f-150",
+        "f-250",
+        "f-350",
+        "ranger",
+        "contour",
+        "escort",
+        "mondeo",
+        "bronco",
+        "excursion",
+        "flex"
+    ],
+    "Lincoln": [
+        "MKZ",
+        "continental",
+        "MKC",
+        "MKT",
+        "MKX",
+        "navigator",
+        "town car",
+        "LS",
+        "aviator",
+        "zephyr",
+        "MKS"
+    ],
+    "Buick": [
+        "LaCross",
+        "regal",
+        "encore",
+        "envision",
+        "enclave",
+        "century",
+        "excelle",
+        "LeSabre",
+        "Lucerne",
+        "park avenue",
+        "roadmaster",
+        "rendezvous",
+        "rainier",
+        "skylark",
+        "terraza"
+    ],
+    "Cadillac": [
+        "ATS",
+        "CTS",
+        "XTS",
+        "CT6",
+        "XT5",
+        "escalade"
+    ],
+    "Chevrolet": [
+        "spark",
+        "sonic",
+        "bolt",
+        "volt",
+        "cruze",
+        "malibu",
+        "impala",
+        "SS",
+        "caprice",
+        "camaro",
+        "corvette",
+        "express",
+        "trax",
+        "equinox",
+        "traverse",
+        "tahoe",
+        "suburban",
+        "colorado",
+        "silverado",
+        "lumina",
+        "cavalier",
+        "CMP",
+        "CMV",
+        "cobalt",
+        "corsa",
+        "montana",
+        "monte carlo"
+    ],
+    "GMC": [
+        "sonoma",
+        "sierra",
+        "canyon",
+        "suburban",
+        "yukon",
+        "envoy",
+        "acadia",
+        "terrain"
+    ],
+    "Tesla": [
+        "model s",
+        "model x",
+        "model 3"
+    ],
+    "Pontiac": [
+        "aztek",
+        "bonneville",
+        "firebird",
+        "g2",
+        "g3",
+        "g4",
+        "g5",
+        "g6",
+        "grand am",
+        "grand prix",
+        "GTO",
+        "montana",
+        "solstice",
+        "sunbird",
+        "sunfire",
+        "torrent",
+        "trans-am",
+        "vibe",
+        "wave"
+    ],
+    "Toyota": [
+        "4Runner",
+        "avalon",
+        "camry",
+        "century",
+        "highlander",
+        "land cruiser",
+        "prius",
+        "sequoia",
+        "sienna",
+        "tacoma",
+        "tundra",
+        "yaris",
+        "corolla",
+        "rav4",
+        "venza",
+        "verossa",
+        "vista",
+        "pickup"
+    ],
+    "Lexus": [
+        "CT",
+        "IS",
+        "HS",
+        "ES",
+        "GS",
+        "LS",
+        "SC",
+        "RC",
+        "LC",
+        "LFA",
+        "NX",
+        "RX",
+        "GX",
+        "LX"
+    ],
+    "Acura": [
+        "TSX",
+        "MDX",
+        "integra",
+        "RL",
+        "TL",
+        "RDX",
+        "CL",
+        "CSX",
+        "EL",
+        "legend",
+        "ZDX",
+        "NSX",
+        "CDX",
+        "vigor"
+    ],
+    "Nissan": [
+        "skyline",
+        "maxima",
+        "sentra",
+        "pathfinder",
+        "altima",
+        "cube",
+        "murano",
+        "rogue",
+        "versa",
+        "GT-R",
+        "370Z",
+        "350Z",
+        "leaf",
+        "juke",
+        "200SX",
+        "xterra"
+    ],
+    "Honda": [
+        "accord",
+        "civic",
+        "cr-v",
+        "fit",
+        "legend",
+        "pilot",
+        "passport",
+        "prelude",
+        "cr-x"
+    ],
+    "Mazda": [
+        "mazda2",
+        "mazda3",
+        "mazda5",
+        "mazda6",
+        "mx-5",
+        "cx-3",
+        "cx-5",
+        "cx-9",
+        "mx-6",
+        "rx-8",
+        "mpv"
+    ],
+    "Subaru": [
+        "forester",
+        "impreza",
+        "legacy",
+        "outback"
+    ],
+    "Mitsubishi": [
+        "360",
+        "380",
+        "500",
+        "3000GT",
+        "outlander",
+        "lancer",
+        "galant",
+        "lancer evolution",
+        "carisma",
+        "pajero",
+        "eclipse",
+        "endeavor",
+        "magna",
+        "ek",
+        "diamante",
+        "GTO",
+        "freeca",
+        "FTO",
+        "mirage",
+        "challenger"
+    ],
+    "kia": [
+        "forte",
+        "optima",
+        "soul",
+        "sorento",
+        "sportage",
+        "spectra",
+        "pride",
+        "sephia"
+    ],
+    "hyundai": [
+        "accent",
+        "aero",
+        "dynasty",
+        "elantra",
+        "genesis",
+        "G80",
+        "i10",
+        "i20",
+        "i30",
+        "i40",
+        "sante fe",
+        "sonata",
+        "tucson",
+        "velostar",
+        "veracruz"
+    ],
+    "Isuzu": [
+        "trooper",
+        "wizard",
+        "ascender",
+        "aska",
+        "amigo"
+    ],
+    "Audi": [
+        "A1",
+        "A2",
+        "A3",
+        "A4",
+        "A5",
+        "A6",
+        "A7",
+        "A8",
+        "TT",
+        "Q3",
+        "Q5",
+        "Q7",
+        "R8",
+        "allroad"
+    ],
+    "BMW": [
+        "1 series",
+        "2 series",
+        "3 series",
+        "4 series",
+        "5 series",
+        "6 series",
+        "7 series",
+        "x1",
+        "x3",
+        "x4",
+        "x5",
+        "x6",
+        "z1",
+        "z3",
+        "z4",
+        "z8",
+        "i3",
+        "i8"
+    ],
+    "Mercedes": [
+        "a class",
+        "b class",
+        "c class",
+        "cl class",
+        "clk class",
+        "cls class",
+        "e class",
+        "g class",
+        "gl class",
+        "m class",
+        "r class",
+        "sl class",
+        "slk class"
+    ],
+    "Porsche": [
+        "911",
+        "boxster",
+        "cayman",
+        "cayenne",
+        "panamera"
+    ],
+    "Volkswagen": [
+        "golf",
+        "jetta",
+        "beetle",
+        "passat",
+        "scirocco",
+        "tiguan",
+        "touareg",
+        "corrado",
+        "bus"
+    ],
+    "Fiat": [
+        "500",
+        "panda",
+        "punto",
+        "freemont"
+    ],
+    "Mini": [
+        "cooper",
+        "countryman",
+        "clubman"
+    ],
+    "Jaguar": [
+        "f-type",
+        "xe",
+        "xf",
+        "xj",
+        "s-type",
+        "x-type"
+    ],
+    "Land Rover": [
+        "evoque",
+        "range rover",
+        "discovery",
+        "freelander",
+        "defender"
+    ]
+}
+
+export const MAKE_OPTIONS = Object.keys(MODEL_OPTIONS).sort()
+
+export const ALL_MODEL_OPTIONS = Object.entries(MODEL_OPTIONS)
+    .reduce((options, [make, models]) => options.concat(models), [])
+    .sort()
